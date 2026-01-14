88954e5c75a82c8426a8e96e17e81113d3dd5777
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2026-01-14 01:45:05 +0100

    ajout dep + modif config api
    ajout de class-transformer, config du cors pour tester via swagger, ajou config validator pipe

--- apps/realtime-elo-ranker-server/src/main.ts
+++ apps/realtime-elo-ranker-server/src/main.ts
@@ -1,9 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({ origin: [
+    'http://localhost:3000', //allow local postman client
+    'http://localhost:3001', // allow local swagger
+    'http://151.80.147.102:3001', // allow vps swagger (online)
+    'http://localhost:3002', // allow local frontend dev server
+  ] 
+  }); // allow Postman client, swagger local, and vps swagger
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
